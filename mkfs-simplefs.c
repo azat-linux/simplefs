daab07c88e7d7c57ea89e699480a13cd0de94754
Azat Khuzhin <a3at.mail@gmail.com>
2014-07-12 17:38:22 +0400

    mkfs: split into more helpers (and drop malloc just do seek instead)

--- mkfs-simplefs.c
+++ mkfs-simplefs.c
@@ -9,35 +9,14 @@
 
 #include "simple.h"
 
-int main(int argc, char *argv[])
+const uint64_t WELCOMEFILE_DATABLOCK_NUMBER = 3;
+const uint64_t WELCOMEFILE_INODE_NUMBER = 2;
+
+static int write_superblock(int fd)
 {
-	int fd, nbytes;
-	ssize_t ret;
 	struct simplefs_super_block sb;
-	struct simplefs_inode root_inode;
-	struct simplefs_inode welcomefile_inode;
+	ssize_t ret;
 
-	char welcomefile_name[] = "vanakkam";
-	char welcomefile_body[] = "Love is God. God is Love. Anbe Murugan.\n";
-	const uint64_t WELCOMEFILE_INODE_NUMBER = 2;
-	const uint64_t WELCOMEFILE_DATABLOCK_NUMBER = 3;
-
-	char *block_padding = NULL;
-
-	struct simplefs_dir_record record;
-
-	if (argc != 2) {
-		printf("Usage: mkfs-simplefs <device>\n");
-		return -1;
-	}
-
-	fd = open(argv[1], O_RDWR);
-	if (fd == -1) {
-		perror("Error opening the device");
-		return -1;
-	}
-
-	/* Begin writing of Block 0 - Super Block */
 	sb.version = 1;
 	sb.magic = SIMPLEFS_MAGIC;
 	sb.block_size = SIMPLEFS_DEFAULT_BLOCK_SIZE;
@@ -50,19 +29,22 @@ int main(int argc, char *argv[])
 	sb.free_blocks &= ~(1 << WELCOMEFILE_DATABLOCK_NUMBER);
 
 	ret = write(fd, (char *)&sb, sizeof(sb));
-
 	if (ret != SIMPLEFS_DEFAULT_BLOCK_SIZE) {
 		printf
 		    ("bytes written [%d] are not equal to the default block size\n",
 		     (int)ret);
-		ret = -1;
-		goto exit;
+		return -1;
 	}
 
 	printf("Super block written succesfully\n");
-	/* End of writing of Block 0 - Super block */
+	return 0;
+}
 
-	/* Begin writing of Block 1 - Inode Store */
+static int write_inode_store(int fd)
+{
+	ssize_t ret;
+
+	struct simplefs_inode root_inode;
 
 	root_inode.mode = S_IFDIR;
 	root_inode.inode_no = SIMPLEFS_ROOTDIR_INODE_NUMBER;
@@ -74,89 +56,118 @@ int main(int argc, char *argv[])
 	if (ret != sizeof(root_inode)) {
 		printf
 		    ("The inode store was not written properly. Retry your mkfs\n");
-		ret = -1;
-		goto exit;
+		return -1;
 	}
 	printf("root directory inode written succesfully\n");
+}
 
-	welcomefile_inode.mode = S_IFREG;
-	welcomefile_inode.inode_no = WELCOMEFILE_INODE_NUMBER;
-	welcomefile_inode.data_block_number = WELCOMEFILE_DATABLOCK_NUMBER;
-	welcomefile_inode.file_size = sizeof(welcomefile_body);
-	ret = write(fd, (char *)&welcomefile_inode, sizeof(root_inode));
+static int write_inode(int fd, const struct simplefs_inode *i)
+{
+	off_t nbytes;
+	ssize_t ret;
 
-	if (ret != sizeof(root_inode)) {
+	ret = write(fd, (char *)&i, sizeof(*i));
+	if (ret != sizeof(*i)) {
 		printf
 		    ("The welcomefile inode was not written properly. Retry your mkfs\n");
-		ret = -1;
-		goto exit;
+		return -1;
 	}
 	printf("welcomefile inode written succesfully\n");
 
-	nbytes =
-	    SIMPLEFS_DEFAULT_BLOCK_SIZE - sizeof(root_inode) -
-	    sizeof(welcomefile_inode);
-	block_padding = malloc(nbytes);
-
-	ret = write(fd, block_padding, nbytes);
-
-	if (ret != nbytes) {
+	nbytes = SIMPLEFS_DEFAULT_BLOCK_SIZE - sizeof(*i) - sizeof(*i);
+	ret = lseek(fd, nbytes, SEEK_CUR);
+	if (ret == (off_t)-1) {
 		printf
 		    ("The padding bytes are not written properly. Retry your mkfs\n");
-		ret = -1;
-		goto exit;
+		return -1;
 	}
+
 	printf
 	    ("inode store padding bytes (after the two inodes) written sucessfully\n");
+	return 0;
+}
+int write_dirent(int fd, const struct simplefs_dir_record *record)
+{
+	ssize_t nbytes = sizeof(*record), ret;
 
-	/* End of writing of Block 1 - inode Store */
-
-	/* Begin writing of Block 2 - Root Directory datablocks */
-	strcpy(record.filename, welcomefile_name);
-	record.inode_no = WELCOMEFILE_INODE_NUMBER;
-	nbytes = sizeof(record);
-
-	ret = write(fd, (char *)&record, nbytes);
+	ret = write(fd, record, nbytes);
 	if (ret != nbytes) {
 		printf
 		    ("Writing the rootdirectory datablock (name+inode_no pair for welcomefile) has failed\n");
-		ret = -1;
-		goto exit;
+		return -1;
 	}
 	printf
 	    ("root directory datablocks (name+inode_no pair for welcomefile) written succesfully\n");
 
-	nbytes = SIMPLEFS_DEFAULT_BLOCK_SIZE - sizeof(record);
-	block_padding = realloc(block_padding, nbytes);
-
-	ret = write(fd, block_padding, nbytes);
-	if (ret != nbytes) {
+	nbytes = SIMPLEFS_DEFAULT_BLOCK_SIZE - sizeof(*record);
+	ret = lseek(fd, nbytes, SEEK_CUR);
+	if (ret == (off_t)-1) {
 		printf
 		    ("Writing the padding for rootdirectory children datablock has failed\n");
-		ret = -1;
-		goto exit;
+		return -1;
 	}
 	printf
 	    ("padding after the rootdirectory children written succesfully\n");
-	/* End of writing of Block 2 - Root directory contents */
+	return 0;
+}
+int write_block(int fd, char *block, size_t len)
+{
+	ssize_t ret;
 
-	/* Begin writing of Block 3 - Welcome file contents */
-	nbytes = sizeof(welcomefile_body);
-	ret = write(fd, welcomefile_body, nbytes);
-	if (ret != nbytes) {
-		printf("Writing welcomefile body has failed\n");
-		ret = -1;
-		goto exit;
+	ret = write(fd, block, len);
+	if (ret != len) {
+		printf("Writing file body has failed\n");
+		return -1;
 	}
-	printf("welcomefilebody has been written succesfully\n");
-	/* End of writing of Block 3 - Welcome file contents */
+	printf("block has been written succesfully\n");
+	return 0;
+}
 
-	ret = 0;
+int main(int argc, char *argv[])
+{
+	int fd;
+	ssize_t ret;
 
-exit:
-	if (block_padding) {
-		free(block_padding);
+	char welcomefile_body[] = "Love is God. God is Love. Anbe Murugan.\n";
+	struct simplefs_inode welcome = {
+		.mode = S_IFREG,
+		.inode_no = WELCOMEFILE_INODE_NUMBER,
+		.data_block_number = WELCOMEFILE_DATABLOCK_NUMBER,
+		.file_size = sizeof(welcomefile_body),
+	};
+	struct simplefs_dir_record record = {
+		.filename = "vanakkam",
+		.inode_no = WELCOMEFILE_INODE_NUMBER,
+	};
+
+	if (argc != 2) {
+		printf("Usage: mkfs-simplefs <device>\n");
+		return -1;
 	}
+
+	fd = open(argv[1], O_RDWR);
+	if (fd == -1) {
+		perror("Error opening the device");
+		return -1;
+	}
+
+	ret = 1;
+	do {
+		if (write_superblock(fd))
+			break;
+		if (write_inode_store(fd))
+			break;
+
+		if (write_inode(fd, &welcome))
+			break;
+		if (write_dirent(fd, &record))
+			break;
+		if (write_block(fd, welcomefile_body, welcome.file_size))
+			break;
+
+		ret = 0;
+	} while (0);
+
 	close(fd);
 	return ret;
 }
